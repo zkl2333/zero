d9ff0807cfadb9cbb0c6e8b76c0efeca3ae7a801
zkl2333 <i@zkl2333.com>
2020-06-29 18:05:23 +0800

    格式化

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ var rpio = require("rpio");
 const POWER_CHECK = 7;
 
 rpio.open(POWER_CHECK, rpio.INPUT);
-console.log("电源 " + (rpio.read(POWER_CHECK)==1 ? "连接" : "断开"));
+console.log("电源 " + (rpio.read(POWER_CHECK) == 1 ? "连接" : "断开"));
 
-let state = rpio.read(POWER_CHECK)
+let state = rpio.read(POWER_CHECK);
 
-function pollcb(pin)
-{
+function pollcb(pin) {
 	rpio.msleep(300);
-	if ( rpio.read(POWER_CHECK) != state ) {
-		state = rpio.read(POWER_CHECK)
-	        console.log("电源 " + (state == 1 ? "连接" : "断开"));
-	}
-	else {
-		return
+	if (rpio.read(POWER_CHECK) != state) {
+		state = rpio.read(POWER_CHECK);
+		console.log("电源 " + (state == 1 ? "连接" : "断开"));
+	} else {
+		return;
 	}
 }
 
